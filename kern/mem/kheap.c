851a521d654de98e072994499611175109354c37
Khaira Bah <Monayaa1637@gmail.com>
2023-11-06 05:09:58 -0800

    calling kmalloc 4 times >_<

--- kern/mem/kheap.c
+++ kern/mem/kheap.c
@@ -72,43 +72,66 @@ void* sbrk(int increment)
 
 void* kmalloc(unsigned int size)
 {
+
 	//TODO: [PROJECT'23.MS2 - #03] [1] KERNEL HEAP - kmalloc()
 	//refer to the project presentation and documentation for details
 	// use "isKHeapPlacementStrategyFIRSTFIT() ..." functions to check the current strategy
+
+	//if(size >= (KERNEL_HEAP_MAX - (hard_limit + PAGE_SIZE)))
+				//	return NULL;
+
+	int numOfPages = ROUNDUP(size,PAGE_SIZE);
+	int num_of_frames = 0;
+	uint32 pa = 0;
+	struct FrameInfo *frame;
+
+
 	if(!isKHeapPlacementStrategyFIRSTFIT())
 		return NULL; //don't know what else to do lol
+
+
 	void* allocated= NULL;
 	if(size<=DYN_ALLOC_MAX_BLOCK_SIZE) //block allocator
 	{
 		allocated = alloc_block_FF(size);
 	}
+
+
 	else //page allocator
 	{
-
-		int numOfPages = ROUNDUP(size,PAGE_SIZE);
-		struct Frame_Info* frame;
-
-		for(int i =0; i<numOfPages;i++)
+		for(uint32 i = 0; i<numOfPages; i++)
 		{
-		/*allocate_frame(&frame);
-		for(i=HARD_LIMIT+PAGE_SIZE; i<KERNEL_HEAP_MAX; i+PAGE_SIZE)//khokho will declare
-		{
-			get_frame_info()
-		}*/
-		/*
-		map_frame(frame, va, PERM_WRITEABLE); //correct permission for kernel?
-		}
-		*/
+				frame = NULL;
+				int is_allocated = allocate_frame(&frame);
+				if(is_allocated == 0)
+				{
+					int is_mapped = map_frame(ptr_page_directory, frame,  (hard_limit + PAGE_SIZE + (i*PAGE_SIZE)), PERM_PRESENT | PERM_USER | PERM_WRITEABLE);
+					if(is_mapped == 0)
+					{
+						num_of_frames++;
+						if(num_of_frames == 1)
+						{
+							 pa = to_physical_address(frame);
+
+						}
+					}
+				}
+
 
 		}
+		allocated = to_frame_info(pa); //hey google cast this & retrun it
 	}
 
 
+
+	cprintf("\nnumber of pages: %d and number of frames: %d\n", numOfPages,num_of_frames);
+
+
 	//change this "return" according to your answer
-	kpanic_into_prompt("kmalloc() is not implemented yet...!!");
-	//kpanic_into_prompt("kmalloc() is not implemented yet...!!");
+	//panic_into_prompt("kmalloc() is not implemented yet...!!");
 	return allocated;
 }
+
 
 void kfree(void* virtual_address)
 {
