f6331b870348deeea46af38f0d27c8023cbd5d44
Amna Ahmed <ammy5342@gmail.com>
2023-11-22 21:40:42 +0200

    Made it add the stack/heap page to page file, still no effect

--- kern/trap/fault_handler.c
+++ kern/trap/fault_handler.c
@@ -90,15 +90,13 @@ void page_fault_handler(struct Env * curenv, uint32 fault_va)
 		//TODO: [PROJECT'23.MS2 - #15] [3] PAGE FAULT HANDLER - Placement
 		// Write your code here, remove the panic and write your code
 		//panic("page_fault_handler().PLACEMENT is not implemented yet...!!");
-		uint32 * page_table;
-		struct FrameInfo *ptr_frame_info = get_frame_info(curenv->env_page_directory,fault_va,&page_table);
 		//int perms = pt_get_page_permissions(curenv->env_page_directory, fault_va);
+		void* va= (void*)fault_va;
 
-
+		struct FrameInfo *ptr_frame_info=NULL;
 		allocate_frame(&ptr_frame_info);
 		map_frame(curenv->env_page_directory,ptr_frame_info,fault_va,PERM_PRESENT|PERM_USER|PERM_WRITEABLE);
 
-		void* va= (void*)fault_va;
 		int ret = pf_read_env_page(curenv,va);
 
 		if (ret == E_PAGE_NOT_EXIST_IN_PF)
@@ -113,16 +111,17 @@ void page_fault_handler(struct Env * curenv, uint32 fault_va)
 			{
 				cprintf("Im killing u tpp");
 				sched_kill_env(curenv->env_id);
-				return;
 			}
 		}
-
-		LIST_INSERT_TAIL(&(curenv->page_WS_list),env_page_ws_list_create_element(curenv, fault_va));
-		if(wsSize+1 < (curenv->page_WS_max_size))
-			curenv->page_last_WS_element = LIST_NEXT(curenv->page_last_WS_element);
 		else
-			curenv->page_last_WS_element = NULL;
-		//refer to the project presentation and documentation for details
+		{
+			LIST_INSERT_TAIL(&(curenv->page_WS_list),env_page_ws_list_create_element(curenv, fault_va));
+			if(wsSize+1 < (curenv->page_WS_max_size))
+				curenv->page_last_WS_element = LIST_NEXT(curenv->page_last_WS_element);
+			else
+				curenv->page_last_WS_element = NULL;
+			//refer to the project presentation and documentation for details
+		}
 	}
 	else
 	{
